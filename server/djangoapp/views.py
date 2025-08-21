1625ec46d808e8cab3e3cae72b3c838bc056903f
RajarshiRayi <rajarshirayi2002@gmail.com>
2025-08-21 03:23:53 -0400

    Django Proxy Module3 Done

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -145,29 +145,12 @@ def get_dealer_details(request, dealer_id):
 
 # Create an `add_review` view to submit a review
 def add_review(request):
-    if request.user.is_authenticated:
+    if(request.user.is_anonymous == False):
         data = json.loads(request.body)
-        print(data)  # Log the incoming data for debugging
         try:
             response = post_review(data)
-            if response.get("status") == 200:
-                print("Review posted successfully:", response)
-            return JsonResponse(
-                {
-                    "status": 200,
-                    "message": "Review posted successfully",
-                    "review": data
-                })
-        except Exception as e:
-            print(f"Error in posting review: {e}")
-            return JsonResponse(
-                {
-                    "status": 401,
-                    "message": "Error in posting review"
-                })
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
     else:
-        return JsonResponse(
-            {
-                "status": 403,
-                "message": "Unauthorized"
-            })
+        return JsonResponse({"status":403,"message":"Unauthorized"})
