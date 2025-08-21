16df3eb744404e05534df8e72cf11f2b6de91bf6
RajarshiRayi <rajarshirayi2002@gmail.com>
2025-08-21 08:19:04 -0400

    Dynamic pages Final project M4

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import LoginPanel from "./components/Login/Login"; // Importing the Login component
 import Register from "./components/Register/Register"; // Importing the Register components
-// import Dealers from './components/Dealers/Dealers';
-// import Dealer from './components/Dealers/Dealer';
-// import PostReview from "./components/Dealers/PostReview";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from './components/Dealers/Dealer';
+import PostReview from "./components/Dealers/PostReview";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,10 +12,10 @@ function App() {
         {/* Route for the Login page */}
         <Route path="/login" element={<LoginPanel />} />
         <Route path="/register" element={<Register />} />
-        {/* 
+        
         <Route path="/dealers" element={<Dealers/>} />
         <Route path="/dealer/:id" element={<Dealer/>} />
-        <Route path="/postreview/:id" element={<PostReview/>} /> */}
+        <Route path="/postreview/:id" element={<PostReview/>} /> 
     </Routes>
   );
 }
